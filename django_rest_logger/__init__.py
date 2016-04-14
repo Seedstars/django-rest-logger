28a8338d37d9c0265922bed17201dcd1fa1380bc
Pedro Gomes <pedro.gomes@seedstarslabs.com>
2016-04-14 09:51:21 +0100

    Update version.

--- django_rest_logger/__init__.py
+++ django_rest_logger/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'django_rest_logger'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __author__ = 'Pedro Gomes'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2016 Seedstars'
