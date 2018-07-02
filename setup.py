93cb3d3b76e8a807a7cdc8ece8bf6bdb3abcbede
Daniel Brown <djbrown@users.noreply.github.com>
2018-07-02 13:04:45 +0200

    loosen restrictions on Django and Python versions

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ author = 'Pedro Gomes'
 author_email = 'pedro.gomes@seedstarslabs.com'
 license = 'MIT'
 install_requires = [
-    'Django>=1.8.0,<1.9.0',
+    'Django>=1.8.7',
 ]
 
 
@@ -107,6 +107,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
